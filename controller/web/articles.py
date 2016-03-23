4c31fb0635591e5042fc49c6769030e1f6a00196
Yangjing Zhang <zhangyangjing@gmail.com>
2016-03-23 12:42:52 +0800

    change title

--- controller/web/articles.py
+++ controller/web/articles.py
@@ -12,10 +12,10 @@ class ArticlesHandler(BaseHandler):
             self._get_special(article_id)
 
     def _get_special(self, article_id):
-        object_id = ObjectId(article_id)
-        d = self.db.articles.find_one({'_id': object_id}, {'data': 1})
+        aid = ObjectId(article_id)
+        d = self.db.articles.find_one({'_id': aid}, {'data': 1, 'title': 1})
         html = self.md.convert(d['data'])
-        self.render('article.html', article=html)
+        self.render('article.html', title=d['title'], article=html)
 
     def _get_all(self):
         d = self.db.articles.find({}, {'_id': 1, 'title': 1})
